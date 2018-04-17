36727c35ac3f39e30dc44273906e567635ff728c
Katherine G. Pe <katherinepe@me.com>
2018-04-18 00:25:24 +1000

    Display persisted data

--- Barangaroo/InfoViewController.swift
+++ Barangaroo/InfoViewController.swift
@@ -12,69 +12,76 @@ import SwiftyJSON
 
 
 class InfoViewController: UIViewController {
+  
+  @IBOutlet weak var barangarooText: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    let sharedSession = URLSession.shared
+    
+    // Details
+    if let detailsUrl = URL(string: "https://api.myjson.com/bins/7wkfr") {
+      let detailsRequest = URLRequest(url: detailsUrl)
+      let dataTask = sharedSession.dataTask(with: detailsRequest, completionHandler: { (data, response, error) -> Void in
         
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let sharedSession = URLSession.shared
+        let context = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "BarangarooInfo", in: context)
+        let bInfo = NSManagedObject(entity: entity!, insertInto: context)
         
-        // Details
-        if let detailsUrl = URL(string: "https://api.myjson.com/bins/7wkfr") {
-            let detailsRequest = URLRequest(url: detailsUrl)
-            let dataTask = sharedSession.dataTask(with: detailsRequest, completionHandler: { (data, response, error) -> Void in
-                
-                let context = appDelegate.persistentContainer.viewContext
-                let entity = NSEntityDescription.entity(forEntityName: "BarangarooInfo", in: context)
-                let bInfo = NSManagedObject(entity: entity!, insertInto: context)
-                
-                guard let content = data else {
-                    print("There seems to be no data")
-                    return
-                }
-                
-                guard let jsonData = (try? JSON(data: content)) else {
-                    print("No JSON data")
-                    return
-                }
-                
-                do {
-                    let placeDescription = jsonData["description"].string!
-                    let link = jsonData["barangaroo"].string!
-                    
-                    bInfo.setValue(placeDescription, forKey: "text")
-                    bInfo.setValue(link, forKey: "link")
-                    
-                    try context.save()
-                } catch {
-                    print("Failed saving")
-                }
-            })
-            
-            
-            dataTask.resume()
+        guard let content = data else {
+          print("There seems to be no data")
+          return
         }
         
-        
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "BarangarooInfo")
-        request.returnsObjectsAsFaults = false
+        guard let jsonData = (try? JSON(data: content)) else {
+          print("No JSON data")
+          return
+        }
         
         do {
-            let context = appDelegate.persistentContainer.viewContext
-            let result = try context.fetch(request)
-            
-            for data in result as! [NSManagedObject] {
-                print("DATA", data)
-            }
+          
+          let placeDescription = jsonData["description"].string!
+          let link = jsonData["barangaroo"].string!
+          
+          bInfo.setValue(placeDescription, forKey: "desc")
+          bInfo.setValue(link, forKey: "link")
+          
+          
+          
+          
+          try context.save()
         } catch {
-            print("Failed to retrieve image")
+          print("Failed saving")
         }
-        
+      })
+      
+      
+      dataTask.resume()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    let request = NSFetchRequest<NSFetchRequestResult>(entityName: "BarangarooInfo")
+    request.returnsObjectsAsFaults = false
+    
+    do {
+      let context = appDelegate.persistentContainer.viewContext
+      let result = try context.fetch(request)
+    
+      
+      barangarooText.numberOfLines = 0
+      
+      for data in result as! [NSManagedObject] {
+        barangarooText.text = data.value(forKey: "desc") as! String
+      }
+    } catch {
+      print("Failed to retrieve image")
     }
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 }
 
